07f5b764c78ec55ffee50702e70643a0507b3c84
anto91github <antoz91@yahoo.com>
2023-12-11 14:20:57 +0700

    create home.blade

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('home', [
+        'name' => 'cara fajar', 
+        'role' => 'admin'
+    ]);
 });
 
 Route::get('/about', function () {
