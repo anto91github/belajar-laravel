187746825aadd632a13bf867fcac0e87dfc03612
anto91github <antoz91@yahoo.com>
2024-01-11 15:08:39 +0700

    get API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\StudentController;
 
 /*
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/testGet', [StudentController::class, 'getAPI']);
+Route::get('/getPosts', [PostController::class, 'index']);
